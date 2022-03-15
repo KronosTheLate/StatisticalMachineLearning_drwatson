63c8769ef0a91fcd865c4f2ea7a19c8ced457ac2
Hubulub <Asbjornpagh@gmail.com>
2022-03-15 13:34:55 +0100

    det skulle jeg

--- scripts/E3_Asbjørn.jl
+++ scripts/E3_Asbjørn.jl
@@ -36,22 +36,19 @@ dataset (you can select any 2 person data for this) using K-means clustering.
 # This defines the data set in a structure, call pictures.ID for id,
 # pictures.class for class
 # pictures.data for data of pictures
-ciphers
-pictures = Picture.( ciphers|>eachrow )
+pictures = Picture.( ciphers|>eachrow );
 
 
 # lets find 2 people
-
 person(ID) = filter(x -> x.ID == ID, pictures) |>remove_constant # returns ID, CLass, and data for person 1
 numbersearch(data, nr) = (filter(x -> x.class == nr, data))
-
 
 
 
 xData = numbersearch(person(12),3)|>datamat  
 
 # cluster X into 20 clusters using K-means
-R = kmeans(xData, 10; maxiter=200, display=:iter);
+R = kmeans(xData, 10; maxiter=200);
 
 a = assignments(R) # get the assignments of points to clusters
 c = counts(R) # get the cluster sizes
@@ -77,3 +74,217 @@ heatmap(fig[2,5],unflatten( M[:,10] )|>reverse|>x->reverse(x, dims=1); colormap,
 hidedecorations!.(fig.content[begin:end])
 fig
 
+##? 
+
+function GetClusterCenter(data::Matrix, numberofcluster::Int64) 
+M = (kmeans(data, numberofcluster; maxiter=200)).centers
+return M
+end
+
+
+#! finder M cluser for person 2 for 0 til 9
+Clustersize = 1;
+temp = []
+for i in 0:9
+M=GetClusterCenter( numbersearch( person(2), i )|>datamat ,Clustersize);
+push!(temp,M)
+end
+temp
+#! Sætter dem sammen i en picture sturct
+
+tempfirst = hcat(repeat(2:2,Clustersize), repeat(0:0,Clustersize), temp[1]')
+for i in 0:9
+temp2 = hcat(repeat(2:2,Clustersize), repeat(i:i,Clustersize), temp[1+i]')
+tempfirst = vcat(tempfirst, temp2)
+end
+Mpictures = Picture.(tempfirst |> eachrow)
+
+
+#! test for accuracy 
+
+function TestAcc(traindata,testdata, k)
+inds, _ = knn(traindata, testdata; k=k, tree=BruteTree, metric=Euclidean());
+preds = classify(inds, traindata.class);
+acc = Statistics.mean(preds .== testdata.class)
+return acc
+end
+
+
+
+TestAcc(person(2), person(5), 1)
+
+##?
+"""
+    cluster_center_matrix(Clustersize::Int, personid::Int)
+
+Builds the center cluster matrix of a single person where numbers 0-9 is clusterd.
+"""
+function cluster_center_matrix(Clustersize::Int, personid::Int) 
+
+    #! finder M cluster for person 2 for 0 til 9
+    let
+    temp = []
+    for i in 0:9
+    M=GetClusterCenter( numbersearch( person(personid), i )|>datamat ,Clustersize);
+    push!(temp,M)
+    end
+    temp
+    #! Sætter dem sammen i en picture sturct
+    
+    tempfirst = hcat(repeat(2:2,Clustersize), repeat(0:0,Clustersize), temp[1]')
+    for i in 0:9
+    temp2 = hcat(repeat(2:2,Clustersize), repeat(i:i,Clustersize), temp[1+i]')
+    tempfirst = vcat(tempfirst, temp2)
+    end
+    Mpictures = Picture.(tempfirst |> eachrow)
+    return Mpictures
+    end
+end
+
+
+##? 
+#! This is the ploting place ! 
+
+fig = Figure()
+ax = Axis(fig[1,1])
+sca20 = scatter!([1], [TestAcc(cluster_center_matrix(20, 2), person(5), 1)], color=:blue)
+sca10 = scatter!([1], [TestAcc(cluster_center_matrix(10, 2), person(5), 1)],color=:red)
+sca5 = scatter!([1], [TestAcc(cluster_center_matrix(5, 2), person(5), 1)],color=:green)
+scaOG =scatter!([1], [TestAcc(person(2), person(5), 1)], color=:black)
+
+scatter!([2], [TestAcc(cluster_center_matrix(20, 12), person(13), 1)], label = "20 cluster", color=:blue)
+scatter!([2], [TestAcc(cluster_center_matrix(10, 12), person(13), 1)], label = "10 cluster", color=:red)
+scatter!([2], [TestAcc(cluster_center_matrix(5, 12), person(13), 1)], label = "5 cluster", color=:green)
+scatter!([2], [TestAcc(person(12), person(13), 1)], label = "KNN no clustering", color=:black)
+
+scatter!([3], [TestAcc(cluster_center_matrix(20, 4), person(8), 1)], label = "20 cluster", color=:blue)
+scatter!([3], [TestAcc(cluster_center_matrix(10, 4), person(8), 1)], label = "10 cluster", color=:red)
+scatter!([3], [TestAcc(cluster_center_matrix(5, 4), person(8), 1)], label = "5 cluster", color=:green)
+scatter!([3], [TestAcc(person(4), person(8), 1)], label = "KNN no clustering", color=:black)
+
+
+scatter!([4], [TestAcc(cluster_center_matrix(20, 20), person(25), 1)], label = "20 cluster", color=:blue)
+scatter!([4], [TestAcc(cluster_center_matrix(10, 20), person(25), 1)], label = "10 cluster", color=:red)
+scatter!([4], [TestAcc(cluster_center_matrix(5, 20), person(25), 1)], label = "5 cluster", color=:green)
+scatter!([4], [TestAcc(person(20), person(25), 1)], label = "KNN no clustering", color=:black)
+
+
+Legend(fig[1, 2],
+    [sca20, sca10, sca5, scaOG],
+    ["20 clusters", "10 clusters", "5 clusters", "KNN no clustering"])
+
+processors = ["2 vs. 1", "12 vs. 13", "4 vs. 8", "20 vs. 25"]
+#sca20.ylabel = "Hello"
+ax.xticks = (1:4, processors)
+fig
+
+#! seems highly dependet on the people you chose to compare as to which is better... strange. 
+
+##? Cross validation
+
+ function crossvald(clustersize, clusterperson, testperson, k, rep)
+accSaver = []
+for i in 1:10
+push!(accSaver, TestAcc(cluster_center_matrix(clustersize, clusterperson), testperson, k);)
+end
+return accSaver
+end
+accSaver |> mean
+accSaver |> var |> √
+
+cl20v21 = crossvald(20, 20 , person(21), 1, 1:50)
+cl12v13 = crossvald(20, 12 , person(13), 1, 1:50)
+cl1v2 = crossvald(20, 1 , person(2), 1, 1:10)
+
+cl20v21_10 = crossvald(10, 20 , person(21), 1, 1:10)
+cl12v13_10 = crossvald(10, 12 , person(13), 1, 1:10)
+cl1v2_10 = crossvald(10, 1 , person(2), 1, 1:10)
+
+
+
+
+fig = Figure()
+
+ax=Axis(fig[1,1])
+
+xs = 1:1:3
+ys1 = [mean(cl20v21), mean(cl12v13), mean(cl1v2)]
+#ys2 = [mean(cl20v21_10), mean(cl12v13_10), mean(cl1v2_10)]
+
+lowerrors = [var(cl20v21)|>√, var(cl12v13)|>√, var(cl1v2)|>√]
+higherrors = [var(cl20v21)|>√, var(cl12v13)|>√, var(cl1v2)|>√]
+
+#lowerrors_10 = [var(cl20v21_10)|>√, var(cl12v13_10)|>√, var(cl1v2_10)|>√]
+#higherrors_10 = [var(cl20v21_10)|>√, var(cl12v13_10)|>√, var(cl1v2_10)|>√]
+
+
+errorbars!(xs, ys1, lowerrors, higherrors,
+    color = :green,
+    whiskerwidth = 10)
+
+    
+#errorbars!(xs, ys2, lowerrors_10, higherrors_10,
+#color = :blue,
+#whiskerwidth = 10)
+
+# plot position scatters so low and high errors can be discriminated
+sca = scatter!(xs, ys1, color = :green)
+#scatter!(xs, ys2, color = :blue)
+scaOG = scatter!(xs, [TestAcc(person(20), person(21), 1), TestAcc(person(12), person(13), 1), TestAcc(person(1), person(2), 1)],
+    color = :black
+)
+
+processors = ["20 vs. 21", "12 vs. 13", "1 vs. 2"]
+#sca20.ylabel = "Hello"
+ax.xticks = (1:3, processors)
+
+Legend(fig[1, 2],
+    [sca, scaOG],
+    ["20 Clusters", "KNN no clustering"])
+
+
+fig
+
+
+##? 
+
+##! alt data! 
+
+
+
+tts = TrainTestSplit(pictures |> remove_constant, 1//1, false) 
+
+
+"""
+    cluster_center_matrix(Clustersize::Int, personid::Int)
+
+Builds the center cluster matrix of a single person where numbers 0-9 is clusterd.
+"""
+function cluster_center_matrixAll(Clustersize::Int, data::Vector{Picture{Float64}}) 
+
+    #! finder M cluster for person 2 for 0 til 9
+    let
+    temp = []
+    for i in 0:9
+    M=GetClusterCenter( numbersearch( data, i )|>datamat ,Clustersize);
+    push!(temp,M)
+    end
+    temp
+    #! Sætter dem sammen i en picture sturct
+    
+    tempfirst = hcat(repeat(69:69,Clustersize), repeat(0:0,Clustersize), temp[1]')
+    for i in 1:9
+    temp2 = hcat(repeat(69:69,Clustersize), repeat(i:i,Clustersize), temp[1+i]')
+    tempfirst = vcat(tempfirst, temp2)
+    end
+    Mpictures = Picture.(tempfirst |> eachrow)
+    return Mpictures
+    end
+end
+
+Mpictures = cluster_center_matrixAll(300, tts.train)
+
+TestAcc(Mpictures, tts.test, 1)
+TestAcc(tts.train, tts.test, 1)
+
+Mpictures[91] |> visualize_picture
